7e416b6ba8cb547f2d4c1d1c79698116e7782561
chinaeffrec <sergey.chinaev@gmail.com>
2026-03-28 21:03:16 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,24 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    driver = item.funcargs.get("driver", None)
+
+    if rep.when == "call" and rep.failed and driver:
+        screenshot = driver.get_screenshot_as_png()
+        allure.attach(
+            screenshot,
+            name="screenshot",
+            attachment_type=allure.attachment_type.PNG
+        )
 
 @pytest.fixture
 def driver():
